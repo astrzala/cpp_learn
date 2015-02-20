d50cb17e15772dc234f3c6c669d3b87fb71e2d02
astrzala <4ndrzej@gmail.com>
2015-02-20 18:06:41 +0100

    Hello world

---
+++ back_to_the_roots/back_to_the_roots.cpp
@@ -0,0 +1,13 @@
+// back_to_the_roots.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello world");
+	return 0;
+}
+
